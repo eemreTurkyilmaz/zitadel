d9c0f7ad40f402698ad6131725c101e32791f943
Tuncay Damlar <32389400+tuncaydamlar@users.noreply.github.com>
2024-09-02 15:09:52 +0300

    update

--- console/src/app/app.module.ts
+++ console/src/app/app.module.ts
@@ -15,6 +15,7 @@ import localeZh from '@angular/common/locales/zh';
 import localeRu from '@angular/common/locales/ru';
 import localeNl from '@angular/common/locales/nl';
 import localeSv from '@angular/common/locales/sv';
+import localeTr from '@angular/common/locales/tr';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -102,6 +103,8 @@ registerLocaleData(localeNl);
 i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/nl.json'));
 registerLocaleData(localeSv);
 i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/sv.json'));
+registerLocaleData(localeTr);
+i18nIsoCountries.registerLocale(require('i18n-iso-countries/langs/tr.json'));
 
 export class WebpackTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
--- docs/docs/guides/manage/customize/texts.md
+++ docs/docs/guides/manage/customize/texts.md
@@ -49,6 +49,7 @@ ZITADEL is available in the following languages
 - Russian (ru)
 - Dutch (nl)
 - Swedish (sv)
+- Turkish (tr)
 
 A language is displayed based on your agent's language header.
 If a users language header doesn't match any of the supported or [restricted](#restrict-languages) languages, the instances default language will be used.
